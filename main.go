1173bf7035afbacfffbaab3fbb2b19607bedc7f1
EWestenbroek <entropy@mbp-van-euan.kpn>
2024-08-14 17:02:33 +0200

    fix gosec security issues

--- main.go
+++ main.go
@@ -91,6 +91,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 10 * time.Second, // Set the ReadHeaderTimeout to 10 seconds or your preferred duration
 	}
 
 	log.Printf("Serving on port: %s\n", port)
